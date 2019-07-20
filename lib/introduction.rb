68f535a5f7494d6cfa1654d36609884edbf39d57
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-20 19:49:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,3 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
-introduction("Josh")
-
-def introduction(name,language)
-  puts "Hi, my name is #{name} and I am learning a new #{language}."
-end
