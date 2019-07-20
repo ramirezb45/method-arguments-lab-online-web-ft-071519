1b6f52f9e073cd8b3d9f3c407791437034fa27df
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-20 19:50:40 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction(name,language)
+  puts "Hi, my name is #{name} and I am learning a new #{language}."
+end
+introduction_with_language("Dan", "Ember.js")
